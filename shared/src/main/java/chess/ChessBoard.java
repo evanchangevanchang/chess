294b266059f6f40866c8e4a39a99dea781dd35f2
Evan <evanevan@byu.edu>
2026-05-04 13:28:46 +0200

    Chessboard passed some tests

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -42,7 +42,8 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+
+
     }
 
     @Override
