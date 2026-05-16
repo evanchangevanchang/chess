e91b27856f4384c2c58c270839d5322c98dd452e
Evan <evanevan@byu.edu>
2026-05-16 22:05:41 +0200

    MemoryAuthDAO methods

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -14,4 +14,21 @@ public class MemoryAuthDAO implements AuthDAO{
     public void clearAuth() {
         AUTH_MAP.clear();
     }
+
+    public void createAuth(String username) {
+        String newToken = generateToken();
+        AuthData newAuthData = new AuthData(newToken, username);
+        AUTH_MAP.put(newToken, newAuthData);
+    }
+
+    public AuthData getAuth(String authToken) {
+        return AUTH_MAP.get(authToken);
+    }
+
+    public void deleteAuth(String authToken) throws DataAccessException{
+        if (AUTH_MAP.get(authToken) == null) {
+            throw new DataAccessException("AuthData could not be removed");
+        }
+        AUTH_MAP.remove(authToken);
+    }
 }
