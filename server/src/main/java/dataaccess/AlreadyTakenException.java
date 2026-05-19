b48a65561970c86109e1f62d251ca4658202ad62
Evan <evanevan@byu.edu>
2026-05-20 00:55:47 +0200

    added exceptions

--- server/src/main/java/dataaccess/AlreadyTakenException.java
+++ server/src/main/java/dataaccess/AlreadyTakenException.java
@@ -1,9 +1,6 @@
 package dataaccess;
 
-/**
- * Indicates there was an error connecting to the database
- */
-public class AlreadyTakenException extends Exception{
+public class AlreadyTakenException extends RuntimeException {
     public AlreadyTakenException(String message) {
         super(message);
     }
